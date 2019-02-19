576b5a0c03f74851ba5e492c736957bc22167587
Sebastian Tornow <sebastian.tornow@metafinanz.de>
2019-02-19 16:08:53 +0100

    init repo

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM alpine:3.1
+
+# Update
+RUN apk add --update python py-pip
+
+COPY templates /templates
+COPY static /static
+COPY mv.py /mv.py
+COPY requirements.txt /requirements.txt
+# Install app dependencies
+RUN pip install -r /requirements.txt
+ENV MVG_CHECKER '["marienplatz", "karlsplatz"]'
+
+EXPOSE  5000
+CMD ["python", "/mv.py"]
---
+++ mv.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+from flask import Flask, render_template
+import mvg_api
+import os
+import json
+
+stations = json.loads(os.environ['MVG_CHECKER'])
+template = "main.html"
+
+def get_id(station):
+	sid = mvg_api.get_id_for_station(station)
+	return sid
+
+def get_stuff(string):
+	get = mvg_api.get_departures(get_id(string))
+	summary = {}
+	f=0
+	for i in get:
+		f=f+1
+		summary[f] = {'label': i['label'], 'product': i['product'], 'destination': i['destination'], 'departure': i['departureTimeMinutes']}
+	return summary
+
+app = Flask(__name__)
+
+@app.route("/<any({}):segment>".format(str(stations)[1:-1]))
+def first(segment):
+	return render_template(template, title=segment, dict=get_stuff(segment), station=segment, stations=stations)
+
+if __name__ == "__main__":
+	app.run(host='0.0.0.0', use_reloader=True)
