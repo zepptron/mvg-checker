c078a33fe8d820aacb94ae657ad04ea2e4a84508
Sebastian Tornow <sebastian.tornow@metafinanz.de>
2019-02-20 16:33:46 +0100

    add reload

--- mv.py
+++ mv.py
@@ -18,14 +18,25 @@ def get_stuff(string):
 	f=0
 	for i in get:
 		f=f+1
-		summary[f] = {'label': i['label'], 'product': i['product'], 'destination': i['destination'], 'departure': i['departureTimeMinutes']}
+		summary[f] = {
+			'label': i['label'],
+			'product': i['product'],
+			'destination': i['destination'],
+			'departure': i['departureTimeMinutes']
+		}
 	return summary
 
 app = Flask(__name__)
 
 @app.route("/<any({}):segment>".format(str(stations)[1:-1]))
 def first(segment):
-	return render_template(template, title=segment, dict=get_stuff(segment), station=segment, stations=stations)
+	return render_template(
+		template,
+		title=segment,
+		dict=get_stuff(segment),
+		station=segment,
+		stations=stations
+	)
 
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', port=80, use_reloader=True)
